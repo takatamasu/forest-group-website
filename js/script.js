d672d26be319921801a0c5b3e433f6f179cd1c9a
Asu <asunohanna@gmail.com>
2025-09-16 14:17:55 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const burgerBtn = document.querySelector('.burger-btn')
+const navMobile = document.querySelector('.nav-mobile')
+const body = document.body
+
+const handleNav = () => {
+	navMobile.classList.toggle('nav-mobile--active')
+	body.classList.toggle('no-scroll')
+}
+
+const closeNav = () => {
+	navMobile.classList.remove('nav-mobile--active')
+	body.classList.remove('no-scroll')
+}
+
+document.querySelectorAll('.nav-mobile a').forEach(link => {
+	link.addEventListener('click', closeNav)
+})
+
+const setCurrentYear = () => {
+	const footerYearElement = document.querySelector('.footer__year')
+	if (!footerYearElement) return
+	footerYearElement.textContent = new Date().getFullYear()
+}
+
+setCurrentYear()
+
+// Add Event Listeners
+burgerBtn.addEventListener('click', handleNav)
