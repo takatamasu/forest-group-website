2941e4e3d42d1b575ae3d9a10912d19a80e87b25
Asu <asunohanna@gmail.com>
2025-09-17 17:11:20 +0200

    Add scrollspy on desktop navigation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const burgerBtn = document.querySelector('.burger-btn')
 const navMobile = document.querySelector('.nav-mobile')
 const body = document.body
+const sections = document.querySelectorAll('section, header')
+const navLinks = document.querySelectorAll('.nav-desktop__link')
 
+// mobile nav
 const handleNav = () => {
 	navMobile.classList.toggle('nav-mobile--active')
 	navMobile.classList.add('nav-mobile--animated')
@@ -17,6 +20,24 @@ document.querySelectorAll('.nav-mobile a').forEach(link => {
 	link.addEventListener('click', closeNav)
 })
 
+// scroll spy
+window.onscroll = () => {
+	sections.forEach(section => {
+		let top = window.scrollY
+		let offset = section.offsetTop - 150
+		let height = section.offsetHeight
+		let id = section.getAttribute('id')
+
+		if (top >= offset && top < offset + height) {
+			navLinks.forEach(links => {
+				links.classList.remove('nav-desktop__link--active')
+				document.querySelector('.nav-desktop__link[href*=' + id + ']').classList.add('nav-desktop__link--active')
+			})
+		}
+	})
+}
+
+// current year in footer
 const setCurrentYear = () => {
 	const footerYearElement = document.querySelector('.footer__year')
 	if (!footerYearElement) return
